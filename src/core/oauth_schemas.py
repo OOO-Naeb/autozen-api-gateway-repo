b53de660f80631d8ac85a907f1b3e77dbc8cfc84
Igor Ruzhilov <iruzhilov@gmail.com>
2024-10-14 18:03:40 +0500

    Change OAuth2 token schemas' names.

--- src/core/oauth_schemas.py
+++ src/core/oauth_schemas.py
@@ -2,12 +2,12 @@ from fastapi.security import OAuth2PasswordBearer
 
 from src.core.config import settings
 
-oauth2_access_token_scheme = OAuth2PasswordBearer(
+oauth2_access_token_schema = OAuth2PasswordBearer(
     tokenUrl="api/v1/auth/login",
     scopes=settings.get_parsed_scopes
 )
 
-oauth2_refresh_token_scheme = OAuth2PasswordBearer(
+oauth2_refresh_token_schema = OAuth2PasswordBearer(
     tokenUrl="api/v1/auth/refresh",
     scopes=settings.get_parsed_scopes
 )
